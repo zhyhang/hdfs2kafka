5ef6d08d3be730d6150cdc1bb2ba5d00b68171ef
zhyhang <zhyhang1978@gmail.com>
2017-08-09 18:47:38 +0800

    add kafka producer

--- src/main/java/com/yanhuang/bigdata/kafka/File2Kafka.java
+++ src/main/java/com/yanhuang/bigdata/kafka/File2Kafka.java
@@ -5,18 +5,18 @@ package com.yanhuang.bigdata.kafka;
 
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
-import java.util.Map;
+import java.util.Properties;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
-import java.util.function.BooleanSupplier;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,16 +27,31 @@ import org.slf4j.LoggerFactory;
  *
  */
 public class File2Kafka {
-	
-	private static final String LOG_PREFIX_WHOLE_FILE_ERROR="whole file to kafka error";
+
+	private static final String LOG_PREFIX_WHOLE_FILE_ERROR = "whole file to kafka error";
+
+	private Path exceptionFileRoot = Paths.get("/tmp");
+
+	private String kafkaTopic = "test";
 
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	private ConcurrentMap<Path, BufferedWriter> fileWriterMap = new ConcurrentHashMap<>();
 
-	private Path exceptionFileRoot = Paths.get("/tmp");
-	
-	
+	private Producer<String, String> kafkaProducer;
+
+	{
+		Properties props = new Properties();
+		props.put("bootstrap.servers", "localhost:9092");
+		props.put("acks", "all");
+		props.put("retries", 0);
+		props.put("batch.size", 16384);
+		props.put("linger.ms", 1);
+		props.put("buffer.memory", 33554432);
+		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		kafkaProducer = new KafkaProducer<>(props);
+	}
 
 	/**
 	 * @param args
@@ -45,18 +60,18 @@ public class File2Kafka {
 		Path exceptionFileRoot = Paths.get("/tmp");
 		Path file = Paths.get("/data/logs/part-00000");
 		System.out.println(exceptionFileRoot.resolve("except." + file.toString().replace(File.separatorChar, '.')));
-		
-		File2Kafka toK=new File2Kafka();
-		
+
+		File2Kafka toK = new File2Kafka();
+
 		toK.close();
 	}
-	
-	public void write(String dir,int lookupDepth,Predicate<String> fileLineFilter) {
+
+	public void write(String dir, int lookupDepth, Predicate<String> fileLineFilter) {
 		Path[] files = listFiles(Paths.get(dir), lookupDepth);
 		// serialize deal with one file to another
 		// you can parallel deal with files
-		Stream.of(files).forEach(file->{
-			writeFile(file, fileLineFilter==null?l->true:fileLineFilter);
+		Stream.of(files).forEach(file -> {
+			writeFile(file, fileLineFilter == null ? l -> true : fileLineFilter);
 		});
 	}
 
@@ -69,25 +84,37 @@ public class File2Kafka {
 			return new Path[0];
 		}
 	}
-	
-	private void writeFile(Path file,Predicate<String> fileLineFilter) {
+
+	private void writeFile(Path file, Predicate<String> fileLineFilter) {
 		try {
 			// parallel write file lines，need memory for file data
-			Files.readAllLines(file).stream().unordered().parallel().filter(fileLineFilter).forEach(line->{
-				writeLine(file,line);
+			Files.readAllLines(file).stream().unordered().parallel().filter(fileLineFilter).forEach(line -> {
+				writeLine(file, line);
 			});
 		} catch (Exception e) {
-			logger.error(LOG_PREFIX_WHOLE_FILE_ERROR+" {}",file);
+			logger.error(LOG_PREFIX_WHOLE_FILE_ERROR + " {}", file);
 		}
 	}
-	
+
 	private void writeLine(Path file, String line) {
 		try {
-			
-		}catch(Exception e) {
+			int index001 = line.indexOf('\001');
+			int index002 = line.indexOf('\002');
+			String uuid = null;
+			if (index001 >= 0 && index002 >= 0) {
+				uuid = line.substring(index001 + 1, index002);
+			}
+			if (uuid != null) {
+				ProducerRecord<String, String> record = new ProducerRecord<String, String>(kafkaTopic, uuid, line);
+				kafkaProducer.send(record);
+			} else {
+				writeExceptionLine(file, line);
+			}
+
+		} catch (Exception e) {
 			writeExceptionLine(file, line);
 		}
-		
+
 	}
 
 	private void writeExceptionLine(Path file, String line) {
@@ -119,6 +146,7 @@ public class File2Kafka {
 				logger.error("close filewriter {} error", k, e);
 			}
 		});
+		kafkaProducer.close();
 	}
 
 }
